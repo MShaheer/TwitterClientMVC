286eac5d15cefcdadbab3f41a2df0891f9e07d76
MShaheer <shaheernov123@gmail.com>
2014-04-06 23:41:57 +0500

    First commit.
    A project built after following a tutorial on plural sight. Just for
    reference.

---
+++ TwitterClientMVC/Controllers/Tweets.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TwitterClientMVC.Controllers
+{
+    public class Tweets
+    {
+        public Tweet[] results;
+
+    }
+
+
+    public class Tweet {
+        [JsonProperty("from_user")]
+        public string UserName { get; set; }
+        [JsonProperty("text")]
+        public string TweetText { get; set; }
+     }
+}
